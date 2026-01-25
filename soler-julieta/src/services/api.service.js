8eef5cf61a092b36c01f43051f660fc43326aff6
Julieta Soler <julieta_soler@hotmail.com>
2026-01-25 13:29:51 -0300

    Corrigiendo app.js

--- soler-julieta/src/services/api.service.js
+++ soler-julieta/src/services/api.service.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL
+const BASE_URL = import.meta.env.VITE_API_URL || process.env.REACT_APP_API_URL
 
 export async function call({ uri, method = 'GET', body = undefined, isMultipart = false })
 {
