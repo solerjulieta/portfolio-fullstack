24dca607a89697edaa85940f455eb066d674f9dc
Julieta Soler <julieta_soler@hotmail.com>
2026-01-25 13:51:27 -0300

    Corrigiendo app.js x3

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
+app.use(express.json())
 app.set('view engine', 'ejs') //Le decimos al servidor que vamos a usar EJS como motor de plantillas
 
 app.use(express.urlencoded({ extended: true }))
