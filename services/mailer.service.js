afe203361c71bae3715c9d8b498cf38cb95da09f
Julieta Soler <julieta_soler@hotmail.com>
2026-01-25 19:32:57 -0300

    actualizando el mailer.service

--- services/mailer.service.js
+++ services/mailer.service.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend'
 import 'dotenv/config'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_SECRET_KEY)
 
 async function sendGenericEmail({ to, subject, html, text, replyTo }) {
   try {
