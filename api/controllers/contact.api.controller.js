303f76e81b3de415507a8da27763f1a78b70e280
Julieta Soler <julieta_soler@hotmail.com>
2026-01-25 19:18:58 -0300

    fix: cambiando controller de sendEmail

--- api/controllers/contact.api.controller.js
+++ api/controllers/contact.api.controller.js
@@ -1,5 +1,6 @@
 import * as contactService from '../../services/contact.service.js'
 
+/*
 async function sendEmail(req, res)
 {
     const emailData = req.body
@@ -11,6 +12,20 @@ async function sendEmail(req, res)
         .catch((err) => {
             res.status(500).json({ error: { msg: 'contact_error_server' } })
         })
+}*/
+
+async function sendEmail(req, res) {
+  try {
+    const emailData = req.body
+    await contactService.sendEmail(emailData)
+    res.status(200).json({ msg: 'contact_success' })
+  } catch (err) {
+    console.error("CONTACT API ERROR:", err)
+    res.status(500).json({
+      error: 'contact_error_server',
+      detail: err.message
+    })
+  }
 }
 
 export{
