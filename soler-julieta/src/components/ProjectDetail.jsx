b235466998f6378d79d991f5e31d42cda0ef92f9
Julieta Soler <julieta_soler@hotmail.com>
2026-01-31 19:35:52 -0300

    Actualizando imgs

--- soler-julieta/src/components/ProjectDetail.jsx
+++ soler-julieta/src/components/ProjectDetail.jsx
@@ -1,38 +1,76 @@
 import CompSection from './CompSection'
 import { motion } from 'framer-motion'
 import Subtitle from './Subtitle'
+import { useEffect, useState } from 'react'
+import projectsService from '../services/projects.service'
+import { useParams } from 'react-router-dom'
+import i18n from '../js/i18n'
+import ReactMarkDown from 'react-markdown'
 
 export default function ProjectDetail()
 {
+    const { id } = useParams()
+    const [project, setProject] = useState()
+    const lang = i18n.language || 'es'
+
+    useEffect(() => {
+        projectsService.getById(id)
+        .then(data => {
+            setProject(data)
+            console.log("El subtitulo es", data.caseStudy.subtitle[lang])
+        })
+    }, [id])
+
     return(
         <CompSection className="mt-20">
-            <div className="xl:flex xl:items-start">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Marca" />
-                </div>
-                <div>
-                    <h1 className="text-2xl lg:text-4xl mb-5">Donde empieza la identidad</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-            <div className="flex">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Isologo" />
-                </div>
-                <div>
-                    <h2>Cuando la marca se vuelve forma</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-            <div className="flex">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Color" />
-                </div>
-                <div>
-                    <h2>Donde la emoción toma color</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
+            <motion.h1
+                className="text-2xl lg:text-4xl mb-5"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+            >
+                {project?.title}
+            </motion.h1>
+            {project?.caseStudy?.enabled && 
+                Array.isArray(project.caseStudy.sections) && (
+                    <div className="space-y-16">
+                        {project.caseStudy.sections
+                        .sort((a, b) => a.order - b.order)
+                        .map((section, index) => (
+                            <motion.div
+                                className="xl:flex xl:items-start"
+                                initial={{ opacity: 0, y: 30 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                viewport={{ once: true }}
+                                transition={{ delay: index * 0.1 }}
+                            >
+                                <div className="xl:mr-4">
+                                    <Subtitle subtitle={section.subtitle?.[lang]} />
+                                </div>
+                                <div>
+                                    <h2 className="text-2xl lg:text-4xl mb-6">{section.title?.[lang]}</h2>
+                                    <ReactMarkDown 
+                                    components={{
+                                        p: ({ children }) => (
+                                            <p className="text-txtGrey lg:text-lg mb-4 leading-relaxed">
+                                                {children}
+                                            </p>
+                                        ),
+                                        strong: ({ children }) => (
+                                            <span className="font-bold text-mainViolet">
+                                                {children}
+                                            </span>
+                                        )
+                                    }}
+                                    >
+                                        {section.body?.[lang]}
+                                    </ReactMarkDown>
+                                    <p>{section.content}</p>
+                                </div>
+                            </motion.div>
+                        ))}
+                    </div>
+                )
+            }
         </CompSection>
     )
 
